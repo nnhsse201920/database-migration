edb1b1dc9a9be680a524a904cf70d7f0a0083e8c
msahuja <42580307+msahuja@users.noreply.github.com>
2020-03-03 13:35:42 -0600

    updated docker-compose, made api folder

--- phpmyadmin/api/repository.java
+++
@@ -1,7 +0,0 @@
-import com.springbootdev.domain.entity.organization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizationRepository extends JpaRepository<organization, Long>
-{
-
-}
