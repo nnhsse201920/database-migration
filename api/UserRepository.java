e0ca3d25a5b7848ca4d90d585bb8317f51c06d05
msahuja <42580307+msahuja@users.noreply.github.com>
2020-03-11 13:42:45 -0500

    reordered files

--- api/UserRepository.java
+++
@@ -1,7 +0,0 @@
-import com.springbootdev.domain.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizationRepository extends JpaRepository<User, Long>
-{
-
-}
